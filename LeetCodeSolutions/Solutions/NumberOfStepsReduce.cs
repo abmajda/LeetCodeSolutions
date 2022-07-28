4af76a8331be8a82fef9af376e75fc88dc285796
Andy Majda <abmajda@gmail.com>
2022-07-28 17:03:04 -0400

    finished -AM

---
+++ LeetCodeSolutions/Solutions/NumberOfStepsReduce.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Solutions
+{
+    internal class NumberOfStepsReduce
+    {
+        public int NumberOfSteps(int num)
+        {
+            int length = 0;
+
+            while (num != 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num = num / 2;
+                }
+                else
+                {
+                    num--;
+                }
+
+                length++;
+            }
+
+            return length;
+        }
+    }
+}

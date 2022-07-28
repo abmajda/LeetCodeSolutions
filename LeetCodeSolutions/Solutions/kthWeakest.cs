b6327431febdecf4076bf6947d38c52b7d3185a9
Andy Majda <abmajda@gmail.com>
2022-07-27 20:03:39 -0400

    finished using non linq approach -AM

---
+++ LeetCodeSolutions/Solutions/kthWeakest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Solutions
+{
+    internal class kthWeakest
+    {
+        public int[] KWeakestRows(int[][] mat, int k)
+        {
+            int[] weakest = new int[k];
+            List<KeyValuePair<int, int>> keys = new List<KeyValuePair<int, int>>();
+
+            // create the list
+            for (int i = 0; i < mat.Length; i++)
+            {
+                int soldiers = 0;
+
+                for (var j = 0; j < mat[i].Length; j++)
+                {
+                    if (mat[i][j] == 1)
+                    {
+                        soldiers++;
+                    } 
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                keys.Add(new KeyValuePair<int, int>(i, soldiers));
+            }
+
+            keys.Sort((x, y) =>
+            {
+                if (x.Value == y.Value)
+                {
+                    return x.Key - y.Key;
+                }
+                else
+                {
+                    return x.Value - y.Value;
+                }
+            });
+
+            for (int i = 0; i < k; i++)
+            {
+                weakest[i] = keys[i].Key;
+            }
+
+            return weakest;
+        }
+    }
+}

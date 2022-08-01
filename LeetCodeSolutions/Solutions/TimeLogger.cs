33dc0cc29a3bf8edbe5c6d357f7dedb99e971ba2
Andy Majda <abmajda@gmail.com>
2022-08-01 19:51:47 -0400

    Finished -AM

---
+++ LeetCodeSolutions/Solutions/TimeLogger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Solutions
+{
+    internal class TimeLogger
+    {
+        public TimeLogger()
+        {
+            timeouts = new Dictionary<string, int> ();
+        }
+
+        public bool ShouldPrintMessage(int timestamp, string message)
+        {
+            int value = 0;
+
+            if (timeouts.ContainsKey(message)) 
+            { 
+                value = timeouts[message];
+
+                if (value <= timestamp)
+                {
+                    timeouts[message] = timestamp + 10;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                timeouts.Add(message, timestamp + 10);
+                return true;
+            }
+        }
+
+        private Dictionary<string, int> timeouts;
+    }
+}

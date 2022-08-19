30032181b7e60a5f9b06fd18274b1f4606e90f16
abmajda <60359901+abmajda@users.noreply.github.com>
2022-08-19 19:16:27 -0400

    Finished (that was suprisingly easy) -AM

---
+++ LeetCodeSolutions/Solutions/DesignHitCounter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Solutions
+{
+    internal class HitCounter
+    {
+        private Queue<int> hits;
+
+        public HitCounter()
+        {
+            this.hits = new Queue<int>();
+        }
+
+        public void Hit(int timestamp)
+        {
+            this.hits.Enqueue(timestamp);
+        }
+
+        public int GetHits(int timestamp)
+        {
+
+            while (this.hits.Count != 0 && this.hits.Peek() <= timestamp - 300)
+            {
+                this.hits.Dequeue();
+            }
+
+            return this.hits.Count;
+        }
+    }
+}
